0903ddc71d645946ce979f5a3a2dd26627e44414
456sasa <2651586469@qq.com>
2021-03-16 10:15:52 +0800

    更新鼠标操作

--- 鼠标操作.py
+++ 鼠标操作.py
@@ -10,9 +10,28 @@ ActionChains类提供了鼠标操作的常用方法：
 
 from selenium import webdriver
 from selenium.webdriver import ActionChains
+import time
 
 driver=webdriver.Chrome()
 driver.get("http://www.baidu.com")
 
 #定位要悬停的元素
-above=driver.fin
+above=driver.find_element_by_link_text("新闻")
+#鼠标悬停
+ActionChains(driver).move_to_element(above).perform()
+
+ActionChains(driver).double_click(above).perform()
+
+
+time.sleep(3)
+driver.quit()
+"""
+ActionChains(driver)   
+调用ActionChains类，把浏览器驱动driver作为参数传入
+
+move_to_element(above)
+move_to_element()方法用于模拟鼠标移动到元素上，在调用时需要指定元素
+
+perform（）
+提交所有ActionChains类中存储的行为
+"""
